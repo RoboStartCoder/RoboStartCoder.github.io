f5bc0002664442f20a18210824a4f6db923bd76c
Robo_Start <138618375+RoboStartCoder@users.noreply.github.com>
2023-08-22 12:29:04 +0300

    test

---
+++ script.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAnYfYegoVfYALbJJB3QDYqqWaNX43TsxQ",
+    authDomain: "jsmasanger.firebaseapp.com",
+    projectId: "jsmasanger",
+    storageBucket: "jsmasanger.appspot.com",
+    messagingSenderId: "387502780279",
+    appId: "1:387502780279:web:700916d871874356effb72",
+    measurementId: "G-LMBQJJYW5N"
+  };
+
+let database = app.database();
+console.log(database);
